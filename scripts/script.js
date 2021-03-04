32a9b8c30bb256d09d9dfd975e463e726f62ecba
大塚阿希斗 <happy4seeker9@gmail.com>
2021-03-04 11:49:36 +0900

    コーディング完了

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.js-menu').on('click', function() {
+    $('.js-slidemenu').addClass('is-open');
+    $('.js-mask').addClass('is-open');
+  });
+  $('.js-close').on('click', function() {
+    $('.js-slidemenu').removeClass('is-open');
+    $('.js-mask').removeClass('is-open');
+  });
+});
