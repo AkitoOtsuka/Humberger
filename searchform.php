7f6380b122739801e6eb4d1ea4a35804ce341997
大塚阿希斗 <happy4seeker9@gmail.com>
2021-04-09 14:14:20 +0900

    Revert "archiveループ完成"
    This reverts commit 66a64f42bb1128237decda2c153db3cef952662d.

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php echo esc_url( home_url('/') ); ?>" method="get" autocomplete="off" class="p-header-inner__search-container">
-    <div class="p-header-inner__search-field">
-        <i class="fas fa-search c-icon__search p-header-inner__search-icon"></i>
-        <input type="text" name="s" id="s" value="<?php the_search_query(); ?>">
-     </div>
-    <p class="c-btn__search p-header-inner__search-btn"><input type="submit" name="search-btn" value="検索" id="search-btn"></p>
-</form>
